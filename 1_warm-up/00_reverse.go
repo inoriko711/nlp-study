f819f7feb41f281fe264fe082584fcd818ef8fd4
Noriko Tsutsui <isandesuyo@gmail.com>
2021-01-02 13:21:36 +0900

    文字列の逆順

---
+++ 1_warm-up/00_reverse.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "stressed"
+	rs := []rune(str)
+	for i, j := 0, len(str)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	fmt.Println(string(rs))
+}
