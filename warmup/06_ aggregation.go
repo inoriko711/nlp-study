553f74c2fa4d45394986de45917cf685215b9a49
Noriko Tsutsui <isandesuyo@gmail.com>
2021-01-14 22:56:51 +0900

    和集合

--- warmup/06_ aggregation.go
+++ warmup/06_ aggregation.go
@@ -1,15 +1,41 @@
 package warmup
 
+import "fmt"
+
 func Aggregation(sentence1, sentence2 string) {
 	strList1 := []rune(sentence1)
 	strList2 := []rune(sentence2)
 	ngram1 := ngram(strList1)
 	ngram2 := ngram(strList2)
 
-	// TODO 和集合を求める
+	fmt.Print("1.06. 和集合: ")
+	fmt.Println(union(ngram1, ngram2))
+
 	// TODO 積集合を求める
+	fmt.Print("1.06. 積集合: ")
+	// fmt.Println(union(ngram1, ngram2))
+
 	// TODO 差集合を求める
-	// TODO seが含まれてるかどうか調べる
+	fmt.Print("1.06. 差集合: ")
+	// fmt.Println(union(ngram1, ngram2))
+
+	// TODO seが含まれてるかどうか調べるs
+}
+func union(ss ...[]string) []string {
+	m := map[string]int{}
+	for _, s := range ss {
+		for _, v := range s {
+			m[v]++ // 出現回数のカウント
+		}
+	}
+	res := []string{}
+	for str, c := range m {
+		if c == 1 {
+			// 出現回数が１回のものだけを抽出
+			res = append(res, str)
+		}
+	}
+	return res
 }
 
 func ngram(runeList []rune) []string {
