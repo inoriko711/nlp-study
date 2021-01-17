56dfaea914e0017e4a9a5fabcc40475cba5902bf
Noriko Tsutsui <isandesuyo@gmail.com>
2021-01-17 23:14:37 +0900

    ランダムに並び替えるプログラムの実装

--- warmup/09_typoglycemia.go
+++ warmup/09_typoglycemia.go
@@ -2,10 +2,14 @@ package warmup
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func Typoglycemia(sentence string) {
+	rand.Seed(time.Now().UnixNano())
+
 	// 単語のリスト
 	words := strings.Fields(sentence)
 
@@ -15,12 +19,22 @@ func Typoglycemia(sentence string) {
 		runeList[i] = []rune(word)
 	}
 
-	// TODO 文字の順序をランダムに並び替えるプログラム
+	// 文字の順序をランダムに並び替えるプログラム
+	for _, r := range runeList {
+		if len(r) > 4 {
+			// ss : シャッフルされる想定のrune
+			ss := r[1 : len(r)-2]
+			rand.Shuffle(len(ss), func(i, j int) { ss[i], ss[j] = ss[j], ss[i] })
+			for k, s := range ss {
+				r[k+1] = s
+			}
+		}
+	}
 
 	// runeを文字列に直したリスト
 	resWordList := make([]string, len(words))
-	for _, rune := range runeList {
-		resWordList = append(resWordList, string(rune))
+	for i, r := range runeList {
+		resWordList[i] = string(r)
 	}
 
 	// 単語を文に戻す
