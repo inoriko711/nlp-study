d85fb8372a877b268a22fab2c488e618a99797cb
Noriko Tsutsui <isandesuyo@gmail.com>
2021-01-31 00:38:31 +0900

    枠組みを作成

--- unixcommand/14_OutputTheFirstNLines.go
+++ unixcommand/14_OutputTheFirstNLines.go
@@ -1,1 +1,17 @@
 package unixcommand
+
+import "fmt"
+
+func OutputTheFirstNLines(n int) {
+	// TODO 実装
+
+	result := checkOutputTheFirstNLines()
+
+	fmt.Printf("2.14. %t\n", result)
+}
+
+// TODO 動作確認
+func checkOutputTheFirstNLines() bool {
+
+	return false
+}
