581c5297f605f7d9d6a41ca0b38f39100d1a2f4e
Noriko Tsutsui <isandesuyo@gmail.com>
2021-01-31 09:18:06 +0900

    headコマンドでの実行内容確認

--- unixcommand/14_OutputTheFirstNLines.go
+++ unixcommand/14_OutputTheFirstNLines.go
@@ -1,17 +1,31 @@
 package unixcommand
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
 
 func OutputTheFirstNLines(n int) {
 	// TODO 実装
 
-	result := checkOutputTheFirstNLines()
+	result := checkOutputTheFirstNLines(n)
 
 	fmt.Printf("2.14. %t\n", result)
 }
 
 // TODO 動作確認
-func checkOutputTheFirstNLines() bool {
+func checkOutputTheFirstNLines(n int) bool {
+	//　コマンド実行
+	out, err := exec.Command("head", fmt.Sprintf("-%d", n), filepath.Join("unixcommand", "popular-names.txt")).Output()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	outList := strings.Split(string(out), "\n")
+	fmt.Println(outList)
 
 	return false
 }
