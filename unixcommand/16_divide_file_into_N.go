f19a76e38ff79c81174bcc0bb4b49b8196309cf0
Noriko Tsutsui <isandesuyo@gmail.com>
2021-02-04 23:10:14 +0900

    コマンドでの実行結果の確認

--- unixcommand/16_divide_file_into_N.go
+++ unixcommand/16_divide_file_into_N.go
@@ -1,6 +1,27 @@
 package unixcommand
 
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"path/filepath"
+)
+
 // https://nlp100.github.io/ja/ch02.html#16-%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%82%92n%E5%88%86%E5%89%B2%E3%81%99%E3%82%8B
 func DivideFileIntoN(n int) {
+	result := checkDivideFileIntoN(n)
 
+	fmt.Printf("2.16. %t\n", result)
+}
+
+func checkDivideFileIntoN(n int) bool {
+	//　コマンド実行
+	out, err := exec.Command("split", "-l", fmt.Sprint(n), filepath.Join(Folder, FileName), filepath.Join(Folder, fmt.Sprintf("%s-split.", FileName))).Output()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	fmt.Println(out)
+	return false
 }
