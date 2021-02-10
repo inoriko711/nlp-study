51b134fb1e39dc65ad14007a162f455ad98c67a4
Noriko Tsutsui <isandesuyo@gmail.com>
2021-02-10 22:26:20 +0900

    コマンドでの実装を記載

--- unixcommand/17_different_strings.go
+++ unixcommand/17_different_strings.go
@@ -1,18 +1,36 @@
 package unixcommand
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
 
 // DifferentString 1列目の文字列の種類（異なる文字列の集合）を求める関数
 func DifferentString() {
 
 	// TODO goでの実装
 
+	fmt.Println("2.17. 1列目の文字列の種類：")
+
 	result := checkDifferentString()
 	fmt.Printf("2.17. %t\n", result)
-
 }
 
 // TODO 実装
 func checkDifferentString() bool {
+	// % cut -f 1 unixcommand/popular-names.txt | sort | uniq
+	cmdstr := fmt.Sprintf("cut -f 1 %s | sort | uniq", filepath.Join(Folder, FileName))
+
+	out, err := exec.Command("sh", "-c", cmdstr).Output()
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	outList := strings.Split(string(out), "\n")
+
+	fmt.Println(outList)
 	return false
 }
