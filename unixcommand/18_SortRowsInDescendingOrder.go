14d9625098fd95f9588c49da48ff3544b98e10ce
Noriko Tsutsui <isandesuyo@gmail.com>
2021-02-14 13:41:07 +0900

    チェック用のメソッドを準備

--- unixcommand/18_SortRowsInDescendingOrder.go
+++ unixcommand/18_SortRowsInDescendingOrder.go
@@ -1,5 +1,32 @@
 package unixcommand
 
-func SortRowsInDescendingOrder() {
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
 
+func SortRowsInDescendingOrder() *[]string {
+
+	result := checkSortRowsInDescendingOrder()
+
+	fmt.Printf("2.18. %t\n", result)
+	return nil
+}
+
+// 答え合わせ用
+func checkSortRowsInDescendingOrder() bool {
+	// % sort -n -k 3 unixcommand/popular-names.txt
+	out, err := exec.Command("sort", "-n", "-k", "3", filepath.Join(Folder, FileName)).Output()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	wants := strings.Split(string(out), "\n")
+
+	fmt.Println(wants)
+	return false
 }
