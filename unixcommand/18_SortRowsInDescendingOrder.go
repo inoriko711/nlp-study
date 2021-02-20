db0dc3bd464c5c70893aafdc4fef26e50b1775c0
Noriko Tsutsui <isandesuyo@gmail.com>
2021-02-20 15:15:35 +0900

    望んだ結果が得られることの確認コード

--- unixcommand/18_SortRowsInDescendingOrder.go
+++ unixcommand/18_SortRowsInDescendingOrder.go
@@ -7,9 +7,12 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"reflect"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/kylelemons/godebug/pretty"
 )
 
 // https://nlp100.github.io/ja/ch02.html#18-%E5%90%84%E8%A1%8C%E3%82%923%E3%82%B3%E3%83%A9%E3%83%A0%E7%9B%AE%E3%81%AE%E6%95%B0%E5%80%A4%E3%81%AE%E9%99%8D%E9%A0%86%E3%81%AB%E3%82%BD%E3%83%BC%E3%83%88
@@ -48,17 +51,19 @@ func SortRowsInDescendingOrder() *[]string {
 
 	sort.Slice(rowByPeopleList, func(i, j int) bool { return rowByPeopleList[i].People > rowByPeopleList[j].People })
 
+	var resList []string
 	for _, r := range rowByPeopleList {
-		fmt.Println(r.row)
+		resList = append(resList, r.row)
 	}
-	result := checkSortRowsInDescendingOrder()
+
+	result := checkSortRowsInDescendingOrder(resList)
 
 	fmt.Printf("2.18. %t\n", result)
 	return nil
 }
 
 // 答え合わせ用
-func checkSortRowsInDescendingOrder() bool {
+func checkSortRowsInDescendingOrder(gotRows []string) bool {
 	// % sort -n -k 3 -r unixcommand/popular-names.txt
 	out, err := exec.Command("sort", "-n", "-k", "3", "-r", filepath.Join(Folder, FileName)).Output()
 	if err != nil {
@@ -66,8 +71,25 @@ func checkSortRowsInDescendingOrder() bool {
 		return false
 	}
 
-	wants := strings.Split(string(out), "\n")
-	wants = revoke(wants, "")
+	wantRows := strings.Split(string(out), "\n")
+	wantRows = revoke(wantRows, "")
+	gotRows = revoke(gotRows, "")
 
-	return false
+	// この問題に関しては人数の値の並びだけ一致してればいい
+	want := getPeopleColumn(wantRows)
+	got := getPeopleColumn(gotRows)
+	if !reflect.DeepEqual(want, got) {
+		fmt.Println(pretty.Compare(want, got))
+		return false
+	}
+	return true
+}
+
+func getPeopleColumn(list []string) []string {
+	var res []string
+	for _, element := range list {
+		columns := strings.Split(string(element), "\t")
+		res = append(res, columns[2])
+	}
+	return res
 }
