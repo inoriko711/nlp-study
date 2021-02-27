41afaca3123739941ae10c584d9db0d1a0840768
Noriko Tsutsui <isandesuyo@gmail.com>
2021-02-27 21:06:20 +0900

    Unixcommandでの実行結果の取得

--- unixcommand/19_Frequency.go
+++ unixcommand/19_Frequency.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 // 各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．
@@ -22,6 +25,19 @@ func Frequency() bool {
 }
 
 func checkFrequency() error {
+	// cut -f 1 popular-names.txt | sort | uniq -c | sort -nr
+	cmdstr := fmt.Sprintf("cut -f 1 %s | sort | uniq -c | sort -nr", filepath.Join(UnixcommandFolder, PopularNamesFileName))
+
+	// 実行結果の取得
+	out, err := exec.Command("sh", "-c", cmdstr).Output()
+	if err != nil {
+		return err
+	}
+
+	want := strings.Split(string(out), "\n")
+
+	fmt.Println(want)
+
 	// TODO 実装する
 	return errors.New("未実装")
 }
