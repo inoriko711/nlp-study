eae8c78aeea9094eb3d5b3d83f984b702fef8937
Noriko Tsutsui <isandesuyo@gmail.com>
2021-03-13 14:50:44 +0900

    フォルダの取得

--- regex/regex.go
+++ regex/regex.go
@@ -1,5 +1,19 @@
 package regex
 
-func Execute() {
+import (
+	"fmt"
+	"os"
+)
 
+var JawikiCoountryJsonGZ string
+
+func init() {
+	dir, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	JawikiCoountryJsonGZ = fmt.Sprintf("%s/regex/jawiki-country.json.gz", dir)
+}
+
+func Execute() {
 }
